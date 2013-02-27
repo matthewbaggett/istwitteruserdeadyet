a0b5aee12eee0a5b04e9514ec2193ebcfa95d606
Matthew Baggett <matthewbaggett@houston.dreamhost.com>
2013-02-27 04:42:59 -0800

    More styling changes

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ require_once("config.php");
 $state = "No";
 $adjectives['No'] 	= array("disastrously", "dismally", "grievously", "horribly", "lamentably", "miserably", "regrettably", "sadly", "unhappily", "unsuccessfully", "Poor Show");
 $adjectives['Yes'] 	= array("Thankfully", "Happilly", "Jolly Good");
-$rand = rand(0,count($adjectives[$state]));
+$rand = rand(0, count($adjectives[$state]));
 $adjective = ucfirst($adjectives[$state][$rand]);
+
+$taglines = array("About blunt as a German Barmaid", "Morbid!");
+$rand = rand(0, count($taglines));
+$tagline = $taglines[$rand];
 
 require("template.phtml");
