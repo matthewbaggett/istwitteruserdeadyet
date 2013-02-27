e6e42263968ccdf28ff220214ad2ab841fbf478f
Matthew Baggett <matthewbaggett@houston.dreamhost.com>
2013-02-27 04:36:15 -0800

    Refactoring code to seperate elements

--- index.php
+++ index.php
@@ -1,27 +1,9 @@
-<html>
-  <?php
-    require_once("config.php");
-  ?>
-  <head>
-    <title>Is <?=$alias?$alias:$username?> Dead Yet?</title>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    <style>
-      body{
-        background-color: #DDDDDD;
-      }
-      .deadyet{
-        font-family: 'Raleway', sans-serif;
-        font-weight: 100;
-        font-size: 40px;
-        text-align: center;
-        position: absolute;
-        top: 50%;
-        margin-top: -30px;
+<?php
+require_once("config.php");
+$state = "No";
+$adjectives['No'] 	= array("disastrously", "dismally", "grievously", "horribly", "lamentably", "miserably", "regrettably", "sadly", "unhappily", "unsuccessfully", "Poor Show");
+$adjectives['Yes'] 	= array("Thankfully", "Happilly", "Jolly Good");
+$rand = rand(0,count($adjectives[$state]));
+$adjective = ucfirst($adjectives[$state][$rand]);
 
-      }
-    </style>
-  </head>
-  <body>
-    <div class="deadyet">No. Sadly.</div>
-  </body>
-</html>
+require("template.phtml");
