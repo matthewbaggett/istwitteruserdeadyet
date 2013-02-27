0e79a31c82f7beb34a6a5d83703b6949058e9872
Matthew Baggett <matthewbaggett@houston.dreamhost.com>
2013-02-27 04:48:54 -0800

    Tidy

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ require_once("config.php");
 $state = "No";
 $adjectives['No'] 	= array("disastrously", "dismally", "grievously", "horribly", "lamentably", "miserably", "regrettably", "sadly", "unhappily", "unsuccessfully", "Poor Show");
 $adjectives['Yes'] 	= array("Thankfully", "Happilly", "Jolly Good");
-$rand = rand(0, count($adjectives[$state]));
+$rand = rand(0, count($adjectives[$state])-1);
 $adjective = ucfirst($adjectives[$state][$rand]);
 
 $taglines = array("About blunt as a German Barmaid", "Morbid!");
-$rand = rand(0, count($taglines));
+$rand = rand(0, count($taglines)-1);
 $tagline = $taglines[$rand];
 
 require("template.phtml");
